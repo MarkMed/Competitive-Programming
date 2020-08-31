1c83e45ec883cdc60929d91686de071107a06c79
Marcos Á. Medina <marcmedi1@hotmail.com>
2020-08-31 19:03:13 -0300

    twoSum with double for and two array
    w/o Map

--- LeetCode/1/TwoSum.js
+++ LeetCode/1/TwoSum.js
@@ -2,20 +2,8 @@ var twoSum = (nums, target) => {
 	let indicesSolution = [-1, -1]
 	let leftPointer = 0
 	let rightPointer = nums.length - 1;
-	let numsMap = new Map();
 	let numsCopy = [...nums];
 
-	for(let [indX, numb] of nums.entries()){
-		let indexList = numsMap.get(numb)
-		if (!indexList) {
-			numsMap.set(numb, [indX]);
-		}
-		else {
-			indexList.push(indX)
-			numsMap.set(numb, indexList);
-
-		}
-	}
 	numsCopy.sort(function(a, b){return a-b});
 
 	while (leftPointer < rightPointer) {
@@ -23,16 +11,24 @@ var twoSum = (nums, target) => {
 		if (twoSum > target) {
 			rightPointer--			 
 		}
-		else if (twoSum < target) {
+        else if (twoSum < target) {
 			leftPointer++
 		}
-		else{
-			indicesSolution[0] = numsMap.get(numsCopy[leftPointer])[0];
-			numsMap.get(numsCopy[leftPointer])[0] = -1
-			indicesSolution[1] = numsMap.get(numsCopy[rightPointer])[0] === -1 ? numsMap.get(numsCopy[rightPointer])[1] : numsMap.get(numsCopy[rightPointer])[0];
+		else {
 			break
 		}
 	}
+	for (let [indX, numb] of nums.entries()) {
+		if ( numb === numsCopy[leftPointer]) {
+			indicesSolution[0] = indX
+			leftPointer = null
+		}
+		else {
+			if (numb === numsCopy[rightPointer]) {
+				indicesSolution[1] = indX
+			}
+		}
+	} 
 
 	return indicesSolution
 };
