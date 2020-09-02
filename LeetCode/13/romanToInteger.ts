9bb90191f2958f88fafc25a53138bce32c8841bd
Marcos Á. Medina <marcmedi1@hotmail.com>
2020-09-02 15:27:18 -0300

    romanToInteger Exercise
    must to fix typescript file

---
+++ LeetCode/13/romanToInteger.ts
@@ -0,0 +1,33 @@
+function romanToInt(s: string): number {
+	const romanNumbers = new Map([
+		["I", 1],
+		["IV", 4],
+		["V", 5],
+		["IX", 9],
+		["X", 10],
+		["XL", 40],
+		["L", 50],
+		["XC", 90],
+		["C", 100],
+		["CD", 400],
+		["D", 500],
+		["CM", 900],
+		["M", 1000]
+	]);
+	let result = 0;
+	let i = 0
+
+	while (i < s.length) {
+		
+		if (!!romanNumbers.get(s[i+1]) && (romanNumbers.get(s[i]) < romanNumbers.get(s[i+1]))) {
+			result += romanNumbers.get(s[i] + s[i+1])
+			i += 2;
+		}
+		else {
+			result += romanNumbers.get(s[i]);
+			i++
+		}
+	}
+
+	return result
+};
