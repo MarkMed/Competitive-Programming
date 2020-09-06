380e5575d0e2cf6d3c337f60ba429eaba73d36a0
Marcos Á. Medina <marcmedi1@hotmail.com>
2020-09-06 01:25:01 -0300

    contest exercise 1 completed

---
+++ LeetCode/1576/modifyString.js
@@ -0,0 +1,21 @@
+var modifyString = (s) => {
+	let stringResult = "";
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] != "?") {
+			stringResult += s[i]
+		}
+		else {
+			let nextLetter = s[i+1];
+			let letterToPut = "a";
+			let prevLetterInResult = (stringResult[stringResult.length-1])?(stringResult[stringResult.length-1]):"";
+			let j = 0
+			while (letterToPut === prevLetterInResult || letterToPut === nextLetter) {
+				j++
+				letterToPut = String.fromCharCode(letterToPut.charCodeAt(0)+j);
+			}
+
+			stringResult += letterToPut;
+		}
+	}
+	return stringResult
+};
