ad9e9ed084128524bcfb3890f0e8f623e578e370
Marcos Á. Medina <marcmedi1@hotmail.com>
2020-09-14 11:05:10 -0300

    special position in Binary Matrix exercise

---
+++ LeetCode/1582/specialPosInBinaryMatrix.ts
@@ -0,0 +1,36 @@
+function numSpecial(mat: number[][]): number {
+	let result = 0;
+
+	for (let row = 0; row < mat.length; row++) {
+		let col = 0;
+		let posiblePositionMap = new Map();
+		posiblePositionMap.set(row, -1);
+		while (col < mat[row].length && posiblePositionMap.get(row) != -2) {
+			const element = mat[row][col];
+			if (element === 1) {
+				if (posiblePositionMap.get(row) === -1) {
+					posiblePositionMap.set(row, col);
+				}
+				else {
+					posiblePositionMap.set(row, -2)
+				}
+			}
+			col++
+		}
+		if (col === mat[row].length && posiblePositionMap.get(row) > -1) {
+			let columnFound = posiblePositionMap.get(row);
+			let rowToFind = 0;
+			let totalSum = 0;
+			while (totalSum <= 1 && rowToFind < mat.length) {
+				totalSum += mat[rowToFind][columnFound]
+				rowToFind++
+			}
+			if (totalSum === 1)	{
+				result++
+			}		
+		}
+
+	}
+
+	return result
+};
