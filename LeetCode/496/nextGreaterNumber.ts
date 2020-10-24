2a4257de8aa7709204e8dd972c25739bbffaf905
Marcos Á. Medina <marcmedi1@hotmail.com>
2020-10-23 21:19:23 -0300

    excercise 496 done

---
+++ LeetCode/496/nextGreaterNumber.ts
@@ -0,0 +1,14 @@
+function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
+	let result: number[] = [];
+
+	for (let num of nums1){
+		let greaterNum = -1
+		for (let i = nums2.length - 1; (i >= 0 && nums2[i] != num); i--){
+			if (nums2[i] > num){
+				greaterNum = nums2[i]
+			}
+		}
+		result.push(greaterNum)
+	}
+	return result
+};
